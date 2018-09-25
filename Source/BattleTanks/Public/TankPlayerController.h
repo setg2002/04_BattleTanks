04d3ea3c5da5a57ff705cdd20f4b83a6c1bef44e
Soren Gilbertson <soren.gilbertson13@gmail.com>
2018-09-25 15:33:52 -0600

    - Set up ticking for TankPlayerController files

--- Source/BattleTanks/Public/TankPlayerController.h
+++ Source/BattleTanks/Public/TankPlayerController.h
@@ -4,19 +4,24 @@
 
 #include "Tank.h"
 #include "CoreMinimal.h"
-#include "TankPlayerController.generated.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // Must be last include
 
 /**
- *
+ * 
  */
 UCLASS()
 class BATTLETANKS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
+private:
 	ATank* GetControlledTank() const;
-
+	
 	virtual void BeginPlay() override;
 
+	virtual void Tick(float DeltaTime) override;
+
+	// Start tank moving barrel so that a shot would hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
